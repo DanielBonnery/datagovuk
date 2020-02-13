d3de4efe34a4760dadc08f21e88a7480a4f0b95c
DanielBonnery <daniel.bonnery@gmail.com>
2020-02-13 15:44:43 +0000

    No information

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,14 @@
     packageStartupMessage(
 "Data will be downloaded now:
 ")
+    
+    
+    
+    if(!file.exists(try(file.path(find.package("datagovuk"),'inst')))){
+      dir.create(file.path(find.package("datagovuk"),'inst'))}
+    
+    if(!file.exists(try(file.path(find.package("datagovuk"),'inst/extdata')))){
+      dir.create(file.path(find.package("datagovuk"),'inst/extdata'))}
 get_data_from_web()
 
   }}
