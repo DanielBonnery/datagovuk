1abd8c296a65a9dda5101008894e3d7b907e7c7e
DanielBonnery <daniel.bonnery@gmail.com>
2020-02-13 15:45:32 +0000

    No information

--- R/get_data_from_web.R
+++ R/get_data_from_web.R
@@ -5,8 +5,5 @@ get_data_from_web<-function(directory=
     data.url<-"https://dataworks.calderdale.gov.uk/download/parish-boundaries/80326e22-bba8-44ac-947d-2cab4ae1a78b/Parish%2520boundary.shp.zip"
     xxxx=try(download.file(url=data.url,destfile = tmpf,method="wget",extra="--random-wait --retry-on-http-error=503"))
     Sys.sleep(sample(10, 1))
-    x=unzip(tmpf,exdir = tempdir())
-    y<-read.fwf(x,width=format.table$length,
-                header=FALSE,
-                colClasses=c("character","numeric","numeric")[format.table$class],fill=TRUE)
+    x=unzip(tmpf,exdir = directory)
 }
