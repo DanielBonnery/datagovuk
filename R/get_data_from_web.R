f5bf00551e1f3fa12d63e9d0ad1eb9ca20e372ae
DanielBonnery <daniel.bonnery@gmail.com>
2020-02-13 15:47:59 +0000

    No information

--- R/get_data_from_web.R
+++ R/get_data_from_web.R
@@ -3,7 +3,5 @@ get_data_from_web<-function(directory=
   file.path(find.package("datagovuk"),'inst/extdata')}else{NULL}){
     tmpf  <-tempfile()
     data.url<-"https://dataworks.calderdale.gov.uk/download/parish-boundaries/80326e22-bba8-44ac-947d-2cab4ae1a78b/Parish%2520boundary.shp.zip"
-    xxxx=try(download.file(url=data.url,destfile = tmpf,method="wget",extra="--random-wait --retry-on-http-error=503"))
-    Sys.sleep(sample(10, 1))
-    x=unzip(tmpf,exdir = directory)
+    downloader::download(url=data.url,dest.file=file.path(directory,"ukparishboudaries.zip"))
 }
